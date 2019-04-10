30ed151db79c9c682329da05d04f53461431d1de
hasanlatif <hasanlatif.pk@gmail.com>
2019-04-11 01:21:15 +0500

    Update pcb_defect_detection.py

--- pcb_defect_detection.py
+++ pcb_defect_detection.py
@@ -1,3 +1,15 @@
+'''
+ author:hasanlatif
+ Email:hasanlaif.pk@gmail.com
+ This code will work if you have python 3 ,dlib and opencv installed to your system.
+ if not just download the setup of python 3 from python.org.
+ install it.
+ 'pip3 install opencv-python' to install opencv
+ and use
+ pip3 insatall dlib to install dlib
+Run this script go the project directory using command prompt and write  ""python  simple_emoji_snap_like_filter_v1.py"
+'''
+
 from skimage import io, feature, color
 import matplotlib.pyplot as plt
 from matplotlib import cm
